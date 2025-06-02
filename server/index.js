3784e62e760915f31673834760f71843d150b947
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-06-02 14:09:23 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,84 +1,72 @@
-// import express from "express";
-// import { randomUUID } from "node:crypto";
-// import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-// import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
-// import { isInitializeRequest } from "@modelcontextprotocol/sdk/types.js"
+import express from "express";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { createPost } from "./mcp.tools";
+import { z } from "zod";
+
+const server = new McpServer({
+    name: "example-server",
+    version: "1.0.0"
+});
+
+// ... set up server resources, tools, and prompts ...
+
+const app = express();
 
 
+server.tool(
+    "addTwoNumbers",
+    "Add two numbers",
+    {
+        a: z.number(),
+        b: z.number()
+    },
+    async (arg) => {
+        const { a, b } = arg;
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: `The sum of ${a} and ${b} is ${a + b}`
+                }
+            ]
+        }
+    }
+)
 
-// const app = express();
-// app.use(express.json());
+server.tool(
+    "createPost",
+    "Create a post on X formally known as Twitter ", {
+    status: z.string()
+}, async (arg) => {
+    const { status } = arg;
+    return createPost(status);
+})
 
-// // Map to store transports by session ID
-// const transports: { [sessionId: string]: StreamableHTTPServerTransport } = {};
 
-// // Handle POST requests for client-to-server communication
-// app.post('/mcp', async (req, res) => {
-//   // Check for existing session ID
-//   const sessionId = req.headers['mcp-session-id'] as string | undefined;
-//   let transport: StreamableHTTPServerTransport;
+// to support multiple simultaneous connections we have a lookup object from
+// sessionId to transport
+const transports = {};
 
-//   if (sessionId && transports[sessionId]) {
-//     // Reuse existing transport
-//     transport = transports[sessionId];
-//   } else if (!sessionId && isInitializeRequest(req.body)) {
-//     // New initialization request
-//     transport = new StreamableHTTPServerTransport({
-//       sessionIdGenerator: () => randomUUID(),
-//       onsessioninitialized: (sessionId) => {
-//         // Store the transport by session ID
-//         transports[sessionId] = transport;
-//       }
-//     });
+app.get("/sse", async (req, res) => {
+    const transport = new SSEServerTransport('/messages', res);
+    transports[ transport.sessionId ] = transport;
+    res.on("close", () => {
+        delete transports[ transport.sessionId ];
+    });
+    await server.connect(transport);
+});
 
-//     // Clean up transport when closed
-//     transport.onclose = () => {
-//       if (transport.sessionId) {
-//         delete transports[transport.sessionId];
-//       }
-//     };
-//     const server = new McpServer({
-//       name: "example-server",
-//       version: "1.0.0"
-//     });
+app.post("/messages", async (req, res) => {
+    const sessionId = req.query.sessionId;
+    const transport = transports[ sessionId ];
+    if (transport) {
+        await transport.handlePostMessage(req, res);
+    } else {
+        res.status(400).send('No transport found for sessionId');
+    }
+});
 
-//     // ... set up server resources, tools, and prompts ...
-
-//     // Connect to the MCP server
-//     await server.connect(transport);
-//   } else {
-//     // Invalid request
-//     res.status(400).json({
-//       jsonrpc: '2.0',
-//       error: {
-//         code: -32000,
-//         message: 'Bad Request: No valid session ID provided',
-//       },
-//       id: null,
-//     });
-//     return;
-//   }
-
-//   // Handle the request
-//   await transport.handleRequest(req, res, req.body);
-// });
-
-// // Reusable handler for GET and DELETE requests
-// const handleSessionRequest = async (req: express.Request, res: express.Response) => {
-//   const sessionId = req.headers['mcp-session-id'] as string | undefined;
-//   if (!sessionId || !transports[sessionId]) {
-//     res.status(400).send('Invalid or missing session ID');
-//     return;
-//   }
-  
-//   const transport = transports[sessionId];
-//   await transport.handleRequest(req, res);
-// };
-
-// // Handle GET requests for server-to-client notifications via SSE
-// app.get('/mcp', handleSessionRequest);
-
-// // Handle DELETE requests for session termination
-// app.delete('/mcp', handleSessionRequest);
-
-// app.listen(3000);
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001");
+});
