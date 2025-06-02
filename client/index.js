d31d8145b2339cd097738ba829c59d2d2d4bc5d8
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-06-02 11:18:12 +0530

    client setup done

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const readline = require('readline/promises');
 const  { GoogleGenAI } = require("@google/genai");
+const { text } = require('stream/consumers');
+const { type } = require('os');
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
@@ -14,5 +16,36 @@ const rl = readline.createInterface({
 });
 
 async function chatLoop() {
-    const question = await rl.question('You: ')
+    const question = await rl.question('You: ');
+
+    chatHistory.push({
+        role: "user",
+        parts: [
+            {
+                text: question,
+                type: "text"   
+            }
+        ]
+    })
+
+    const response = await ai.models.generateContent({
+        model:"gemini-2.0-flash",
+        contents: chatHistory
+    })
+   
+    const responseText = response.candidates[0].content.parts[0].text
+   
+        chatHistory.push({
+        role: "model",
+        parts: [
+            {
+                text: responseText,
+                type: "text"
+            }
+        ]
+    })
+     console.log(`AI: ${responseText}`);
+    chatLoop()
 }
+
+chatLoop()
