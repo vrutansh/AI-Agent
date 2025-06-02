4060e29f671530846ac9d03f4ec84304ae97c2f2
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-06-02 10:41:02 +0530

    client setup

--- client/index.js
+++ client/index.js
@@ -1,1 +1,18 @@
+require('dotenv').config();
 const readline = require('readline/promises');
+const  { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+
+
+const chatHistory = [];
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+async function chatLoop() {
+    const question = await rl.question('You: ')
+}
